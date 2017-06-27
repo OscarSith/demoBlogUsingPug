2d613fd7502305f18f0bcf1bff1c1e24be12c135
Oscar Larriega <larriega@gmail.com>
2017-06-27 14:09:16 -0500

    Change home design, add interior html

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,11 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=1614489195537932";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 var prevArrowButton = '<button type="button" class="slick-prev"><i class="glyphicon glyphicon-menu-left"></i></button>';
 var nextArrowButton = '<button type="button" class="slick-next"><i class="glyphicon glyphicon-menu-right"></i></button>';
 
